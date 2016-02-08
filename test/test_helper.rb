0750b9e612bd230e19cdd331f6bc71b0f4910917
Eddie Fisher <eddi.fisher@googlemail.com>
2016-02-08 20:16:29 +0300

    colorize output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,11 +11,54 @@ rescue LoadError
 end
 
 require "minitest/autorun"
+require 'minitest/reporters'
 require "active_record"
 require "factory_girl"
 require "yaml"
 
 DATABASE = ENV["DATABASE"] || "sqlite"
+module Minitest
+  module Reporters
+    class AwesomeReporter < DefaultReporter
+      GRAY = '0;36'
+      GREEN = '1;32'
+      RED = '1;31'
+
+      def initialize(options = {})
+        super
+        @slow_threshold = options.fetch(:slow_threshold, nil)
+      end
+
+      def record_pass(test)
+        if @slow_threshold.nil? || test.time <= @slow_threshold
+          super
+        else
+          gray('O')
+        end
+      end
+
+      def color_up(string, color)
+        color? ? "\e\[#{ color }m#{ string }#{ ANSI::Code::ENDCODE }" : string
+      end
+
+      def red(string)
+        color_up(string, RED)
+      end
+
+      def green(string)
+        color_up(string, GREEN)
+      end
+
+      def gray(string)
+        color_up(string, GRAY)
+      end
+    end
+  end
+end
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::AwesomeReporter.new(reporter_options)]
+
 
 ActiveRecord::Base.establish_connection YAML.load_file(File.expand_path("../database.yml", __FILE__))[DATABASE]
 
